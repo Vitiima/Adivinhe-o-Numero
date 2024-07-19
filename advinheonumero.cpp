876c4f248a11049d5ef6faccf542de0c208169ab
Vitor Matheus Almeida <matheus_famas@hotmail.com>
2024-07-18 21:05:39 -0300

    ;

--- advinheonumero.cpp
+++ advinheonumero.cpp
@@ -6,37 +6,41 @@
 using namespace std;
 advinheonumero::advinheonumero() {
     srand(time(0));
-    inter = rand() % 1000;
+    inter = rand() % 1000 +1;
 }
 
 void advinheonumero::promp() {
     int palp;
-    while (cin >> palp) {
+    if (cin >> palp) {
         if (palp > inter) {
             cout << "Muito Alto, Tente Novamente" << endl;
             promp();
             return;
         }
         if (palp < inter) {
-            cout <<"Muito Baixo Tente Novamente" << endl;
+            cout << "Muito Baixo Tente Novamente" << endl;
             promp();
             return;
         }
         cout << "Parabens voce advinhou o Numero!" << endl;
-        break;
     }
     return;
 }
 void advinheonumero::play() {
     char choice;
-    cout <<"Bem vindo ao Advinhe o numero, Qual seu palpite?"<<endl;
     do {
+        inter = rand() % 1000 + 1;
+        cout << "Bem-vindo ao 'Adivinhe o numero'! Qual e o seu palpite?" << endl;
         promp();
-        cout << "Deseja jogar novamente? Se sim Digite Y"<< endl;
+        cout << "Deseja jogar novamente? Se sim, digite Y. Para sair, digite qualquer outra tecla." << endl;
         cin >> choice;
-    } while (choice == 'y' | choice == 'Y');
-    cout << "Obrigado Por Jogar!" << endl;
-    return;
+        while (cin.fail() || (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N')) {
+            cin.clear();
+            cout << "Entrada inválida. Deseja jogar novamente? Se sim, digite Y. Para sair, digite qualquer outra tecla." << endl;
+            cin >> choice;
+        }
+    } while (choice == 'y' || choice == 'Y');
+    cout << "Obrigado por jogar!" << endl;
 }
 
 
